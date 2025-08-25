495c62163ec5acd5e5eba70481fd8a7c5c81269d
NazeerBShaik <shaiknazeer1995@gmail.com>
2025-08-26 00:04:01 +0530

    new commit

---
+++ modifyStrings.py
@@ -0,0 +1,81 @@
+#upper case example
+a = "Hello, World!"
+print(a.upper())
+#lower case example
+a = "Hello, World!"
+print(a.lower())
+#remove whitespaces
+a = "  Hello, World!"
+print(a.strip())
+#replace string 
+a = "Hello, World!"
+print(a.replace("H","J"))
+#split string
+a = "Hello, World!"
+print(a.split(","))
+#string cancatenation
+a= "Hello"
+b= "World"
+c= a+b
+print(c)
+#string formatting
+age = 29
+txt = f"My name is nazeer, Iam {29}"
+print(txt)
+#Formatting
+price = 29
+txt = f"The price is {price}"
+print(txt)
+#math operartor
+txt = f"The price is {20 * 30} dollers"
+print(txt)
+
+#string methods
+print(txt.isalnum())
+# print(txt.isalpha())
+# print(txt.isascii())
+# print(txt.isdecimal())
+# print(txt.isdigit())
+# print(txt.isidentifier())
+# print(txt.islower())
+# print(txt.isnumeric())
+# print(txt.isprintable())
+# print(txt.isspace())
+# print(txt.istitle())
+# print(txt.isupper())
+# print(txt.join())
+# print(txt.ljust())
+# print(txt.lower())
+# print(txt.lstrip())
+# print(txt.capitalize())
+# print(txt.casefold())
+# print(txt.center())
+# print(txt.count())
+# print(txt.encode())
+# print(txt.endswith())
+# print(txt.expandtabs())
+# print(txt.find())
+# print(txt.format())
+# print(txt.format_map())
+# print(txt.index())
+# print(txt.maketrans())
+# print(txt.partition())
+# print(txt.replace())
+# print(txt.rfind())
+# print(txt.rindex())
+# print(txt.rjust())
+# print(txt.rpartition())
+# print(txt.rsplit())
+# print(txt.rstrip())
+# print(txt.split())
+# print(txt.splitlines())
+# print(txt.startswith())
+# print(txt.strip())
+# print(txt.swapcase())
+# print(txt.title())
+# print(txt.translate())
+# print(txt.upper())
+# print(txt.z())
+
+
+
