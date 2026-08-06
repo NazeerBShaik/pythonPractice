479b78f691c21649d6515f101865d6cfef1bed6e
NazeerBShaik <shaiknazeer1995@gmail.com>
2026-08-06 18:15:35 +0530

    three files added

---
+++ practice.sql
@@ -0,0 +1,12 @@
+create table customers
+(
+    id int,
+    first_name varchar,
+    last_name varchar,
+    age int,
+    gender varchar
+
+)
+insert into customers values
+(1, nazeer, basha, 29, M),
+(2, karthik, gudluru, 32, F)
